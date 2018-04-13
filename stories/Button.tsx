bb6db353ce6103acd77d8c0e3a5d715b7cfedf82
Aaron Rice <adrice727@gmail.com>
2018-04-13 11:30:34 -0700

    Working knobs.

--- stories/Button.tsx
+++ stories/Button.tsx
@@ -2,11 +2,15 @@ import * as React from 'react';
 import { addDecorator, storiesOf } from '@storybook/react';
 
 import Button from '../src/components/Button';
-// import { MuiDecorator } from './mui-decorator';
 
-// addDecorator(MuiDecorator);
+import { withKnobs, text, boolean, number, array, select } from '@storybook/addon-knobs/react';
 
-storiesOf('Button', module)
-  .add('action', () => (
-    <Button kind="link" label="hello tim" onClick={() => null} />
-  ));
+const stories = storiesOf('Button', module);
+
+stories.add('kinds', () => {
+  const name = text('label', 'Hello Tim');
+  const kind = select('kind', { action: 'action', link: 'link' }, 'action');
+  return <Button kind={kind} label={name} />
+});
+
+
