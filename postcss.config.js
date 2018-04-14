455405b997090331c6918a351f00506a240c0ab0
Aaron Rice <adrice727@gmail.com>
2018-04-13 19:03:23 -0700

    Include google in postcss font foudries.

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,22 @@
 module.exports = {
-    plugins: {
-        'postcss-import': {},
-        'postcss-cssnext': {},
-        'stylelint': {},
-        'precss': {},
-        'postcss-nested': {},
-        'postcss-font-magician': {
-            variants: {
-                'Raleway': {
-                    '300': [],
-                    '400': [],
-                    '700': []
-                }
-            }
+  plugins: {
+    'postcss-import': {},
+    'postcss-cssnext': {},
+    'stylelint': {},
+    'precss': {},
+    'postcss-nested': {},
+    'postcss-font-magician': {
+      foundries: ['google'],
+      variants: {
+        'Raleway': {
+          '300': [],
+          '400': [],
+          '700': []
         },
-    }
+        'Pacifico': {
+          '400': [],
+        }
+      },
+    },
+  }
 }
