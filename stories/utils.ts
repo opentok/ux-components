30e7d0675329225a408531b2ebf47173508ea127
Aaron Rice <adrice727@gmail.com>
2018-04-15 14:10:26 -0700

    Update styles for stories.

--- stories/utils.ts
+++ stories/utils.ts
@@ -4,23 +4,54 @@ import { withInfo } from '@storybook/addon-info';
 export const styles = {
   header: {
     h1: {
+      fontFamily: "sans-serif",
+      fontWeight: "300",
       marginRight: "20px",
-      fontSize: "25px",
+      fontSize: "32px",
       display: "inline"
     },
     body: {
       paddingTop: 0,
-      paddingBottom: 0
+      paddingBottom: 0,
+      marginBottom: 0,
     },
     h2: {
       display: "inline",
-      color: "#999"
+      fontFamily: "sans-serif",
+      fontWeight: "300",
+      color: "#999",
     }
   },
   infoBody: {
+    fontFamily: "sans-serif",
+    fontWeight: "300",
     backgroundColor: "#eee",
-    padding: "0px 5px",
-    lineHeight: "2"
+    padding: "10px",
+    lineHeight: "2",
+    h1: {
+      fontFamily: "sans-serif",
+      fontWeight: "300",
+    },
+    h2: {
+      fontFamily: "sans-serif",
+      fontWeight: "300",
+    },
+  },
+  source: {
+    h1: {
+      fontFamily: "sans-serif",
+      fontWeight: "300",
+      marginTop: "10px",
+    },
+    h2: {
+      fontFamily: "sans-serif",
+      fontWeight: "300",
+    },
+  },
+  propTableHead: {
+    fontFamily: "sans-serif",
+    fontWeight: 300,
+    marginTop: "5px"
   }
 };
 export const wInfo = (text: string) => withInfo({ inline: false, source: true, styles, text, header: true });
