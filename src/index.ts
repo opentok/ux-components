6210d089d68843a6a0adc22e6cd5c932f9dd7873
Aaron Rice <adrice727@gmail.com>
2018-04-17 09:13:34 -0700

    Export required Terminal prop types.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import Button from './components/Button';
 import Code from './components/Code';
-import Terminal from './components/Terminal';
+import Terminal, { ITerminalContent } from './components/Terminal';
 
 export {
   Button,
   Code,
-  Terminal
+  Terminal,
+  ITerminalContent
 };
