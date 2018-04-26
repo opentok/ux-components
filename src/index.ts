f1f65971d3c1612ff88a82e1614acaa72db390f4
Aaron Rice <adrice727@gmail.com>
2018-04-26 10:23:26 -0700

    Use variables for fonts. Cleanup.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+/** Import our font faces once at top level */
+import "./styles/imports/font-face.css";
+
+/** Components and any useful types  */
 import Button from './components/Button';
 import Code from './components/Code';
 import Terminal, { ITerminalContent } from './components/Terminal';
